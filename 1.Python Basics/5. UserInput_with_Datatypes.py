02d41b83689a2d114c66e2fd1692af0d09c35706
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-02 00:01:05 +0600

    Auto commit

---
+++ 1.Python Basics/5. UserInput_with_Datatypes.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ") #take user input
+print(name) #user input will print
+
+
+age = int(input("Enter your age:"))
+print("My age is: ",age)
+
+
+length = float(input("Enter length:"))
+print("Length is: ",length)
