4513fed1f441302c87bc2c5deea32a03b06af06b
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-02 01:30:35 +0600

    Auto commit

---
+++ 2. Operators and Operands/1.Arithmetic_Operator.py
@@ -0,0 +1,11 @@
+print(8+2)
+print(8-2)
+print(8*2)
+print(8/2)
+print(8%2) #modulus
+print(8//3)
+'''8 / 3 gives 2.666... (normal division)
+8 // 3 gives only the whole number part → 2
+// is called floor division → it cuts off the decimal and keeps the front part only'''
+
+print(2**4) #exponential
