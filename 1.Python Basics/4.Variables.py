a34570692d66b93851ee8ec349a1e0f74505032d
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-01 22:30:47 +0600

    Auto commit

---
+++ 1.Python Basics/4.Variables.py
@@ -0,0 +1,11 @@
+a = "Hello"
+print(a)
+
+Name = "Saikat"
+print(Name)
+#if print(name) it doesn't print, it will throw error as python is case sensitive
+
+age_of_Saikat = 23
+print(age_of_Saikat)
+#underscore is used to write more than 1 word.. also we can write ageOfSaikat.
+#variables name can't start with number or other special symbols. First character must be letter
