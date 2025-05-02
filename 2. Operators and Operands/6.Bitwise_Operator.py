a9865a9c17b36b7d54cdba0f361899f74a88cbdd
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-02 11:01:12 +0600

    Auto commit

---
+++ 2. Operators and Operands/6.Bitwise_Operator.py
@@ -0,0 +1,14 @@
+print(bin(10))  #bin() is used to find binary
+
+a=10
+b=8
+print(a & b)
+print(a | b)
+print(a ^ b)
+
+
+a=3<<2
+print(a) #bitwise left shift
+
+b=3>>2
+print(b)  #bitwise right shift
