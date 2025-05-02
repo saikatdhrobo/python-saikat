24d49e5140d9764ce60240cbb55c12fa1044aab6
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-02 09:12:35 +0600

    Auto commit

---
+++ 2. Operators and Operands/4.Assignment_Operators.py
@@ -0,0 +1,4 @@
+score = 0
+score += 10 # got 10 points
+score -= 3 # lost 3 points
+print("Final score:", score)
---
+++ Important PDFs/gitpush.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "E:\Data Analysis\Python"
+git add .
+git commit -m "Auto commit"
+git push
+pause
