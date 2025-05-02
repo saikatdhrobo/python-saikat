9d8b9212b03cbd1f52fb45572c7d7bcf10a376ea
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-02 11:25:11 +0600

    Auto commit

---
+++ 3.Conditional Statements/3.if-elif-else.py
@@ -0,0 +1,9 @@
+mark =80
+if mark>=90:
+    print("Go to trip")
+elif mark>=80 and mark<90:
+    print("You get a mobile")
+elif mark>=70 and mark<80:
+    print("You get a book")
+else:   #no condition written inside else statement
+    print("You will not get anything!😒")
