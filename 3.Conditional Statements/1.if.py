ad4c4e3687f036e866a6902595ebb3198bc2d280
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-02 11:16:34 +0600

    Auto commit

---
+++ 3.Conditional Statements/1.if.py
@@ -0,0 +1,7 @@
+mark=87
+if(mark>=90):
+    print("You will get A+")
+print("Thank you")
+
+''' answer will be only "Thank you" because his mark is not greater than or equal to 90. 
+    So it doesn't print "You will get A+" '''
