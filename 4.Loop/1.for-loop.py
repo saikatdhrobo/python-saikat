0ca217589d0d9dbe221579f361b243591375f300
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-02 20:03:52 +0600

    Auto commit

---
+++ 4.Loop/1.for-loop.py
@@ -0,0 +1,25 @@
+1. #count from 1 to 10
+
+for i in range(1,11):
+    print(i)
+
+#2. print a message 5 times
+
+for i in range(5):
+    print("Hello Saikat!")
+
+
+# 3. Count from 6 to 1
+
+for i in range(6,0,-1):
+    print(i)
+
+
+# 4.Even and odd from 11 to 100 
+'''for odd'''
+for i in range(11,101,2):
+    print(i)
+
+'''for even'''
+for j in range(12,101,2):
+    print(j)
