9d2d8abc1378cacc42d8e21f0527756f55a80e0d
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-05 00:04:33 +0600

    Auto commit

---
+++ 6.String Slicing/slice.py
@@ -0,0 +1,18 @@
+a ="Harry Potter and the Goblet of Fire"
+b="0123456789"
+
+
+print(a)
+print(b)
+print(b[::2])
+print(b[:7:3])
+print(a[0:5])
+print(a[6:12])
+
+print(a[:5])
+print(a[-4:])
+
+
+print(b[::-1])
+print(b[6::-1])
+
