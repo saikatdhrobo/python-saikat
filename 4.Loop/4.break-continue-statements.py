0d62e00a21dad8c5961e6768e4e21563482f9a18
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-04 20:22:55 +0600

    Auto commit

---
+++ 4.Loop/4.break-continue-statements.py
@@ -0,0 +1,14 @@
+#1 to 5, skip 4
+'''skipping possible with continue statement'''
+for i in range(1,6):
+    if i==4:
+        continue
+    print(i)
+
+
+# loop stops when 5 prints
+
+for i in range(1,11):
+    print(i)
+    if i==5:
+        break
