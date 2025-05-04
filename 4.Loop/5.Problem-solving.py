50c5f5f64333e0aec6eb1d0399abf15aa236d041
saikatdhrobo <dhrobosaikat4@gmail.com>
2025-05-04 21:00:48 +0600

    Auto commit

---
+++ 4.Loop/5.Problem-solving.py
@@ -0,0 +1,35 @@
+# #1. Write a program to find a sum of all the even numbers up to 50.
+# sum=0
+# for i in range(2,51,2):
+#     sum= sum+i
+# print(sum)
+
+
+# #2. Write a program to write first 20 numbers and their squared numbers.
+
+# for i in range(1,21):
+#     print(i**2)
+
+
+# #3. Write a program to find sum of first 1O odd numbers using while loop.
+
+# sum=0
+# n=0
+# while n<=20:
+#     if n%2!=0:
+#         sum = sum+n
+#     n=n+1
+# print(sum)
+
+
+
+# #4. check if a no. is divisible by 8 and 12, upto 100 numbers
+
+# for i in range(1,101):
+#     if i%8==0 and i%12==0:
+#         print(i)
+
+
+
+#5. Write a program to create a billing system at supermarket
+
