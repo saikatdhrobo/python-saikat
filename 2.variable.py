c028018c08642bac8736d2f132969127dc74e76c
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-05-13 21:06:25 +0600

    Initial commit

---
+++ 2.variable.py
@@ -0,0 +1,52 @@
+# Strings Variable
+
+first_name = "Saikat"
+food = "Cake"
+email = "dhrobosaikat4@gmail.com"
+print(first_name) 
+#f-string
+
+print(f"Hello {first_name}")
+print(f"You like {food}.")
+print(f"Your email is: {email}")
+
+
+
+# Integer Variable (whole numbers)
+
+age = 25
+quantity = 3
+num_of_student = 100
+print(f"You are {age}years old.")
+print(f"You are buying {quantity} items.")
+print(f"Your class has {num_of_student} students.")
+
+
+
+
+
+# Float Variable (decimal numbers)
+
+price = 10.99
+cgpa = 3.21
+distance = 5.5
+print(f"The price is: {price} $")
+print(f"My CG is: {cgpa}")
+print(f"The distance is: {distance} km")
+
+
+
+
+# Boolean Variable (True or False)
+
+is_student = True
+print(f"Are you a student? {is_student}")
+
+
+for_sale = True
+
+if for_sale:
+    print("This item is for sale.")
+else:
+    print("This item is not for sale.")
+
