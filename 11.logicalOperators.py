4a3a5e03bd4cdf502ba5ccb0a7fdf3e0bd5f9d76
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-05-14 10:40:00 +0600

    Auto commit from batch script

---
+++ 11.logicalOperators.py
@@ -0,0 +1,30 @@
+# and or not are logical operators = used on codnitional statements
+
+"""and"""
+temp = 25
+if temp>0 and temp<30:
+    print("The temperature is good")
+else:
+    print("The temperature is bad")
+
+
+
+"""or"""
+temp = 25
+if temp<=0 or temp>=30:
+    print("The temperature is good")
+else:
+    print("The temperature is bad")
+
+
+
+"""not"""
+temp = 20
+sunny = True
+if not sunny:
+    print("it's cloudy outside")
+else:
+    print("it's sunny outside")
+
+
+
