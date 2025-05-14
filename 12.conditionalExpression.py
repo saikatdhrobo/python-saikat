9179785eef0c7f5f3d573d35389f9758c93dbdbd
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-05-14 10:49:26 +0600

    Auto commit from batch script

---
+++ 12.conditionalExpression.py
@@ -0,0 +1,28 @@
+# X if condition else Y
+
+"""example 1"""
+num = -5
+print("Positive" if num > 0 else "Negative")
+
+
+"""example 2"""
+num = 6
+result = "even" if num%2==0 else "odd"
+print(result)
+
+
+
+"""example 3"""
+a = 6
+b = 7
+max_num = a if a>b else b
+min_num = a if a<b else b
+print(max_num)
+print(min_num)
+
+
+
+"""example 4"""
+temperature = 10
+weather = "hot" if temperature >20 else "cold"
+print(weather)
